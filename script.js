7514ed2ba603f9f5c6bb7f101c5e00e69f7f3c4f
Giulio09 <pallotta.gp@gmail.com>
2017-12-04 17:24:11 +0100

    Add files via upload
    Checkbox function added

--- script.js
+++ script.js
@@ -14,20 +14,59 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	$.getJSON(
 		"http://dati.lazio.it/catalog/api/action/datastore_search?resource_id=e2151b09-7350-4fc3-b7e3-592d3d271833&limit=100",
 		function(data) {
+
+			var attivitas = {},
+				markers = [];
+
 			data.result.records.forEach(function(record) {
+
 				var lat = record.lat
 				var lon = record.lon
+				
+
 				if(lat&&lon){
-					L.marker([lat,lon])
-						.bindTooltip(
-							"<p>" + record["DENOMINAZIONE IMPIANTO SPORTIVO"] + "</p>" +
-							"<p>" + record["ATTIVITA"]
-						)
-						.addTo(mymap);
-					console.log(record) 
+
+					record["ATTIVITA"].split(",").forEach(function(attivita) {
+						attivitas[attivita] = (attivitas[attivita] || 0) + 1;
+					});
+
+					markers.push({
+						attivita: record["ATTIVITA"],
+						marker: L.marker([lat,lon])
+							.bindTooltip(
+								"<p>" + record["DENOMINAZIONE IMPIANTO SPORTIVO"] + "</p>" +
+								"<p>" + record["ATTIVITA"] + "</p>"
+							)
+							.addTo(mymap)
+					});
 				}
 				
 			});
+
+			console.log(attivitas, d3.entries(attivitas));
+
+			var divs = d3.select("#attivitas").selectAll("div")
+				.data(d3.entries(attivitas).sort(function(a,b) { return a.value - b.value; }).reverse())
+				.enter()
+				.append("div");
+
+			divs.append("input")
+				.attr("type","radio")
+				.attr("class","attivita")
+				.attr("name", "attivita")
+				.on("click", function(d) {
+					console.log(d, markers);
+				});
+
+			divs.append("label")
+				.attr("for", function(d){
+					return d.key.toLowerCase();
+				})
+				.text(function(d){
+					return d.key + " ("+d.value+")";
+				});
+
+				
 		}
 	);
 
